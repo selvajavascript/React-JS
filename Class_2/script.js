2e7990e148eea324a5ca0a927206039be71065d9
Selvaraj <selva.javascript@gmail.com>
2023-04-25 18:03:52 +0530

    v1.0

---
+++ Class_2/script.js
@@ -0,0 +1,12 @@
+/*Function*/
+function mylogin(){
+    //alert("I am a Alert");
+    console.log("I am a Function()");
+}
+mylogin()
+
+/*Arrow Function
+let mylogin1 = ()=>{
+    console.log("I am a Arrow Function()");
+}
+mylogin1()
